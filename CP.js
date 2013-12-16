4b7e5469c8816b11e5d9eba7a2d6b196d2ae50c0
lenonhere <lenon.here@gmail.com>
2013-12-16 11:16:26 +0800

    ver 0.0002

--- CP.js
+++ CP.js
@@ -7,26 +7,55 @@ String.prototype.format = function() {
 };
 
 var analysisLine = function(strLine) {
-    var pattern = /\(\s*(.*)\s*,\s*(\d*)\s*,\s*(.*)\s*,\s*(.*)\s*,\s*(\d*)\s*,\s*(.*)\s*,\s*(\d*)\s*,\s*(.*)\s*,\s*(.*)\s*,\s*(.*)\s*\),\s*/i;
+    var pattern = /\s*(-{2,})?\s*\(\s*(.*)\s*,\s*(\d*)\s*,\s*(.*)\s*,\s*(.*)\s*,\s*(\d*)\s*,\s*(.*)\s*,\s*(\d*)\s*,\s*(.*)\s*,\s*(.*)\s*,\s*(.*)\s*\),\s*/i;
     var res = pattern.exec(strLine);
     return res;
 };
 
 var generateFormatLine = function(element, fmt) {
+    // for (var i = 1; i <= 11; i++) {
+    //     console.log("the [{0}] is : {1}".format(i, element[i]));
+    // };
+    if(element[1] == undefined) element[1] = "";
     return fmt.format(element[1], element[2], element[3],  
         element[4], element[5], element[6],  
         element[7], element[8], element[9],  
-        element[10]);
+        element[10], element[11]);
 };
 
-var str = "( 'mainid',          0,      'mainid',           '',     1, 'center',   0,'label'    ,'true',   '00'),";
-// var str = "( 'mainid',      0, ";
-var result = analysisLine(str);
-for (var i = 1; i <= 10; i++) {
-    console.log(result[i]);
-};
+var generateFormatBlock = function (block, fmt) {
+    var output = "";
+    var i = 0;
+    console.log(block);
+    if (fmt == undefined) {
+        fmt = "{0}( {1},\t\t\t{2},\t\t\t{3},\t\t\t{4},\t\t\t{5},\t\t\t{6},\t\t{7},\t\t{8},\t\t{9},\t\t{10}),";
+    };
+    while (i < block.length)
+    {
+        var j = block.indexOf("\n", i);
+        if (j == -1) j = block.length;
+        var str = block.substr(i, j - i);
+        var result = analysisLine(str);
+        var resLine = generateFormatLine(result, fmt);
+        console.log("the [{0}] is : \n {1}".format(i, resLine));
+        output += resLine + "\n";
+        i = j + 1;
+    }
+    return output;
+}
+
+// var str = "( 'mainid',          0,      'mainid',           '',     1, 'center',   0,'label'    ,'true',   '00'),";
+// // var str = "( 'mainid',      0, ";
+// var result = analysisLine(str);
+// for (var i = 1; i <= 10; i++) {
+//     console.log(result[i]);
+// };
 // console.log(result);
 
-var fmt = "( {0},\t\t\t{1},\t\t\t{2},\t\t\t{3},\t\t\t{4},\t\t\t{5},\t\t\t{6},\t\t\t{7},\t\t\t{8},\t\t\t{9}),";
-var fmtRes = generateFormatLine(result, fmt);
-console.log(fmtRes);
+var fmt = "{0}( {1},\t\t\t{2},\t\t\t{3},\t\t\t{4},\t\t\t{5},\t\t\t{6},\t\t\t{7},\t\t\t{8},\t\t\t{9},\t\t\t{10}),";
+// var fmtRes = generateFormatLine(result, fmt);
+// var fmtRes = generateFormatBlock(result, fmt);
+// console.log(fmtRes);
+
+
+exports.generateFormatBlock = generateFormatBlock;
---
+++ socket-server.js
@@ -0,0 +1,19 @@
+var generateFormatBlock = require("./CP.js").generateFormatBlock;
+
+var io = require("socket.io").listen(9999);
+
+io.sockets.on("connection", function(socket) {
+    socket.emit("news", {
+        hello: "world"
+    });
+    socket.on("my other event", function(data) {
+        console.log(data);
+    });
+    socket.on("startformat", function(data) {
+        console.log("startformat called!");
+        // console.log(data);
+        var toCode = generateFormatBlock(data.code);
+        console.log(toCode);
+        socket.emit("formatCompleted", { outputCode: toCode});
+    });
+});
